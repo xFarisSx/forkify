997bbc8fb08277afdae41d6fe05104805a1f884d
xFarisSx <faristaleek@gmail.com>
2022-11-19 18:58:59 +0300

    Cleaned up

--- src/js/controller.js
+++ src/js/controller.js
@@ -115,10 +115,6 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature = function () {
-  console.log('Welcome to the application!');
-};
-
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.renderMessage();
@@ -128,6 +124,5 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 init();
