5e7441acfdf47c20b0bd383cb8c7aef335c8f272
xFarisSx <faristaleek@gmail.com>
2022-11-19 18:30:52 +0300

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -115,6 +115,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application!');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.renderMessage();
@@ -124,6 +128,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome!');
+  newFeature();
 };
 init();
